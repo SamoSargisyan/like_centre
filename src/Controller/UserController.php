48d1a06d83939e9e6d546988cfaf3399cf9f68ee
Samson <samvel.sargisyan@bk.ru>
2020-10-29 02:15:45 +0500

    Controller is preliminarily done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Service\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController
+class UserController extends AbstractController
 {
     private UserManager $userManager;
 
@@ -18,5 +19,70 @@ class UserController
     {
         $this->userManager = $userManager;
     }
-    
+
+    /**
+     * @Route("/user/update/{id<\d+>}", name="update_user")
+     *
+     * @param $id
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function update ($id, Request $request): Response
+    {
+
+        $user = $this->userManager->get($id);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $this->userManager->update($user);
+
+            return $this->redirectToRoute('show_user', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/create", name="create_user")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function create (Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $this->userManager->store($user);
+
+            return $this->redirectToRoute('show_user', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/show/{id<\d+>}", name="show_user")
+     *
+     * @param $id
+     *
+     * @return Response
+     */
+    public function show ($id): Response
+    {
+        $user = $this->userManager->get($id);
+
+        return $this->render('user/show.html.twig', compact('user'));
+    }
 }
